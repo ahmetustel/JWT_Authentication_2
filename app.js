847f917957c302b91c390fb51e0e8cd030bb83d4
ahmetustel <ustelahmett@gmail.com>
2023-01-30 22:10:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//const UserRoutes = require('./routes/user.route'); // İhtiyaç halinde route yapısını ekleriz
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+/* This is the root route. It is used to check if the server is running. */
+app.get("/", (req, res) => {
+    res.status(200).json({ alive: "True" });
+  });
+
+/* Telling the server to use the routes in the UserRoutes file. */
+app.use("/", UserRoutes);
+
+
+module.exports = app;
